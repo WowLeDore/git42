e74d31623aeddbcc9650939fcde014e4f4598a70
mguillot <mguillot@student.42.fr>
2025-03-24 22:51:08 +0100

    Fractol wip

--- C2/fract_ol/fractol.c
+++ C2/fract_ol/fractol.c
@@ -14,14 +14,17 @@
 
 int	main(int argc, char **argv)
 {
+	t_mlx	mlx;
+	t_img	img;
+
 	if (argc != 2 || !argv)
-	{
-		ft_printfd(2, "Error. Usage : ./fractol <fractal>\n"
-			"Fractals : mandelbrot (m), julia (j)\n");
-		return (1);
-	}
+		return (1 + 0 * ft_printfd(2, "Error. Usage : ./fractol <fractal>\n"
+				"Fractals : mandelbrot (m), julia (j)\n"));
 	if (argv[1][0] == 'm')
-		ft_printfd(1, "Mandel\n");
-	ft_printfd(1, 0);
+	{
+		mlx.mlx_ptr = mlx_init();
+		mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIDTH, HEIGHT, "Fract'ol");
+		mlx_loop(mlx.mlx_ptr);
+	}
 	return (0);
 }
--- C2/fract_ol/fractol.h
+++ C2/fract_ol/fractol.h
@@ -13,8 +13,24 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# define WIDTH 900
+# define HEIGHT 900
+
 # include <stdarg.h>
 # include <unistd.h>
+# include "minilibx-linux/mlx.h"
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_mlx;
+
+typedef struct s_img
+{
+	void	*img_ptr;
+	char	*img_adr;
+}	t_img;
 
 int	ft_printfd(int fd, const char *format, ...);
 
