6e3f6de81556b5ff12ee56d839309ceb2b20b0b4
mguillot <mguillot@student.42.fr>
2025-03-25 17:09:07 +0100

    Fractol wip

--- C2/fract_ol/fractol.c
+++ C2/fract_ol/fractol.c
@@ -12,10 +12,64 @@
 
 #include "fractol.h"
 
+void	put_pixel(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->img_adr + (y * img->llen + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	comp(double x, double y)
+{
+	int		i;
+	double	re;
+	double	im;
+	double	tmp;
+
+	re = 0.0;
+	im = 0.0;
+	i = -1;
+	while (++i < MAX_ITER)
+	{
+		if (re * re + im * im > 40.0)
+			return (i);
+		tmp = re;
+		re = re * re - im * im + x;
+		im = 2.0 * tmp * im + y;
+	}
+	return (MAX_ITER);
+}
+
+void	mandelbrot(t_mlx *mlx)
+{
+	double	x;
+	double	y;
+	int		iter;
+	int		color;
+
+	x = -1;
+	while (++x < WIDTH)
+	{
+		y = -1;
+		while (++y < HEIGHT)
+		{
+			iter = comp((1 / ZOOM) * (x - WIDTH / 2) + DX,
+					(1 / ZOOM) * (y - HEIGHT / 2) - DY);
+			color = 0x000000FF;
+			if (iter < MAX_ITER)
+				color = 0x7000 * iter / MAX_ITER;
+			put_pixel(mlx->img, x, y, color);
+		}
+	}
+	put_pixel(mlx->img, WIDTH / 2, HEIGHT / 2, 0x00FFFFFF);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr,
+		mlx->img->img_ptr, 0, 0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_mlx	mlx;
-	t_img	img;
 
 	if (argc != 2 || !argv)
 		return (1 + 0 * ft_printfd(2, "Error. Usage : ./fractol <fractal>\n"
@@ -24,6 +78,10 @@ int	main(int argc, char **argv)
 	{
 		mlx.mlx_ptr = mlx_init();
 		mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIDTH, HEIGHT, "Fract'ol");
+		mlx.img->img_ptr = mlx_new_image(mlx.mlx_ptr, WIDTH, HEIGHT);
+		mlx.img->img_adr = mlx_get_data_addr(mlx.img->img_ptr, &mlx.img->bpp,
+				&mlx.img->llen, &mlx.img->endi);
+		mandelbrot(&mlx);
 		mlx_loop(mlx.mlx_ptr);
 	}
 	return (0);
--- C2/fract_ol/fractol.h
+++ C2/fract_ol/fractol.h
@@ -15,22 +15,30 @@
 
 # define WIDTH 900
 # define HEIGHT 900
+# define ZOOM 300.0
+# define DX -0.75
+# define DY 0.0
+# define MAX_ITER 100
 
 # include <stdarg.h>
 # include <unistd.h>
 # include "minilibx-linux/mlx.h"
 
-typedef struct s_mlx
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_mlx;
-
 typedef struct s_img
 {
 	void	*img_ptr;
 	char	*img_adr;
+	int		bpp;
+	int		llen;
+	int		endi;
 }	t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	*img;
+}	t_mlx;
 
 int	ft_printfd(int fd, const char *format, ...);
 
