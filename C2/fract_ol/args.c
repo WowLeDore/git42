d400c3eba7a99e0280b1b96ec0f7adda9081c52a
WowLeDore <maximguillot777@gmail.com>
2025-03-31 16:10:30 +0200

    .

--- C2/fract_ol/args.c
+++ C2/fract_ol/args.c
@@ -6,16 +6,26 @@
 /*   By: mguillot <mguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 13:08:55 by mguillot          #+#    #+#             */
-/*   Updated: 2025/03/29 13:08:56 by mguillot         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:09:40 by mguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	nbr(char **av)
+int	nbr(char *av)
 {
-	(void)av;
-	return (1);
+	if (!av || !*av || *av < '0' || *av > '9')
+		return (0);
+	while (*av >= '0' && *av <= '9')
+		av++;
+	if (*av && *av != '.')
+		return (0);
+	if (!*av)
+		return (1);
+	av++;
+	while (*av >= '0' && *av <= '9')
+		av++;
+	return (!*av);
 }
 
 int	verif(int ac, char **av, int fo)
@@ -23,7 +33,8 @@ int	verif(int ac, char **av, int fo)
 	if (fo == 0)
 		return (ac == 1 && (eq(av[0], "Mandelbrot") || eq(av[0], "m")));
 	else if (fo == 1)
-		return (ac == 3 && (eq(av[0], "Julia") || eq(av[0], "j")) && nbr(++av));
+		return (ac == 3 && (eq(av[0], "Julia") || eq(av[0], "j"))
+			&& nbr(*(++av)) && nbr(*(++av)));
 	else if (fo == 2)
 		return (ac == 1 && (eq(av[0], "Burning-Ship") || eq(av[0], "b")));
 	return (0);
