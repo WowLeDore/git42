41a62b64706e649947475b86c4a81c2acd502871
mguillot <mguillot@student.42.fr>
2025-06-11 15:06:24 +0200

    philo

---
+++ C3/philosophers/opti_philo/parse.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguillot <mguillot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 17:14:43 by mguillot          #+#    #+#             */
+/*   Updated: 2025/06/10 15:13:24 by mguillot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+unsigned int	to_uint(char *str)
+{
+	unsigned int	nbr;
+
+	nbr = 0;
+	while (str && *str)
+		nbr = 10 * nbr + (*(str++) - '0');
+	return (nbr);
+}
+
+int	is_uint(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str && str[len])
+	{
+		if (str[len] < '0' || str[len] > '9')
+			return (0);
+		len++;
+	}
+	return (len < 10 || (len < 11 && str[0] < '5' && to_uint(str) > 999999999));
+}
+
+int	all_uint(int argc, char **argv, t_table *table)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+		if (!is_uint(argv[i++]))
+			return (0);
+	table->number_of_philosophers = to_uint(argv[1]);
+	table->time_to_die = to_uint(argv[2]);
+	table->time_to_eat = to_uint(argv[3]);
+	table->time_to_sleep = to_uint(argv[4]);
+	table->dead = 0;
+	if (argc == 6)
+		table->philosophers_must_eat = 1;
+	else
+		table->number_of_times_must_eat = 0;
+	if (argc == 6)
+		table->number_of_times_must_eat = to_uint(argv[5]);
+	else
+		table->philosophers_must_eat = 0;
+	return (1);
+}
+
+t_errors	parse(int argc, char **argv, t_table *table)
+{
+	if (argc < 5 || argc > 6 || !argv)
+		return (ARGC);
+	if (!all_uint(argc, argv, table))
+		return (FORMAT);
+	if (table->number_of_philosophers == 0)
+		return (PHILO);
+	return (OK);
+}
+
+int	error(t_errors error)
+{
+	if (error == OK)
+		return (0);
+	if (error == ARGC)
+		write(2, MSG_ARGC, 37);
+	else if (error == FORMAT)
+		write(2, MSG_FORMAT, 47);
+	else if (error == PHILO)
+		write(2, MSG_PHILO, 48);
+	else if (error == ALLOC)
+		return (1 + 0 * write(2, MSG_ALLOC, 33));
+	else if (error == MUTEX)
+		return (1 + 0 * write(2, MSG_MUTEX, 46));
+	else if (error == THREAD)
+		return (1 + 0 * write(2, MSG_THREAD, 47));
+	else if (error == TIME)
+		return (1 + 0 * write(2, MSG_TIME, 42));
+	write(2, "\n", 1);
+	write(2, MSG_USAGE1, 40);
+	write(2, MSG_USAGE2, 43);
+	write(2, MSG_USAGE3, 44);
+	write(2, "\n", 1);
+	return (1);
+}
---
+++ C3/philosophers/opti_philo/philo.h
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguillot <mguillot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 16:40:43 by mguillot          #+#    #+#             */
+/*   Updated: 2025/06/10 15:13:23 by mguillot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/time.h>
+
+# define MSG_ARGC "Error: Incorrect number of arguments."
+# define MSG_FORMAT "Error: All arguments must be unsigned integers."
+# define MSG_PHILO "Error: There should be at least one philosopher."
+# define MSG_ALLOC "Error: Memory allocation failed.\n"
+# define MSG_MUTEX "Error: A mutex was not initialized correctly.\n"
+# define MSG_THREAD "Error: A thread was not initialized correctly.\n"
+# define MSG_TIME "Error: The gettime has returned an error.\n"
+
+# define MSG_USAGE1 "Usage: ./philo <number_of_philosophers> "
+# define MSG_USAGE2 "<time_to_die> <time_to_eat> <time_to_sleep>"
+# define MSG_USAGE3 " [number_of_times_each_philosopher_must_eat]"
+
+typedef enum e_errors
+{
+	OK,
+	ARGC,
+	FORMAT,
+	PHILO,
+	ALLOC,
+	MUTEX,
+	THREAD,
+	TIME
+}	t_errors;
+
+typedef struct s_philo
+{
+	unsigned int		id;
+	struct s_table		*table;
+	unsigned long long	dead_time;
+	unsigned long long	meals;
+}	t_philo;
+
+typedef struct s_table
+{
+	unsigned int		number_of_philosophers;
+	unsigned int		time_to_die;
+	unsigned int		time_to_eat;
+	unsigned int		time_to_sleep;
+	int					philosophers_must_eat;
+	unsigned int		number_of_times_must_eat;
+	t_philo				*philos;
+	pthread_t			*threads;
+	pthread_mutex_t		*mutexes;
+	unsigned long long	timer;
+	pthread_mutex_t		print;
+	int					dead;
+	pthread_mutex_t		dead_mutex;
+	pthread_mutex_t		meals_mutex;
+}	t_table;
+
+t_errors	parse(int argc, char **argv, t_table *table);
+int			error(t_errors error);
+
+#endif
